fc83634ba0d2396f4c149e86a2e00d9c27639776
HammadSiddique <hammad.siddique01@gmail.com>
2022-06-12 02:22:34 +0500

    Initialize project with react components and react router

---
+++ src/components/InputBook.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const InputBook = () => (
+    <div className="form-container">
+        <h3 className="form-heading">Add a new book</h3>
+        <form>
+            <input className="title"name="title" type='text' placeholder="Book Title" required />
+            <input className="author"name="author" type='text' placeholder="Author" required />
+            <button className="add-btn">Add Book</button>
+        </form>
+    </div>
+);
+
+export default InputBook;
