95cb26a1609269ddd9dc69092293b844eae54221
HammadSiddique <hammad.siddique01@gmail.com>
2022-06-15 12:43:26 +0500

    Add reducers and actions

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -1,18 +1,18 @@
-import React from "react";
-import Book from "./Book";
-import InputBook from "./InputBook";
+import React from 'react';
+import Book from './Book';
+import InputBook from './InputBook';
 
 const bookslist = {
-    category: 'Science Fiction',
-    title: 'Dune',
-    author: 'Frank Herbert'
+  category: 'Science Fiction',
+  title: 'Dune',
+  author: 'Frank Herbert',
 };
 
 const Books = () => (
-    <div className="main-container"> 
-        <Book books={bookslist}/>
-        <InputBook />
-    </div>
-)
+  <div className="main-container">
+    <Book books={bookslist} />
+    <InputBook />
+  </div>
+);
 
 export default Books;
--- src/components/InputBook.jsx
+++ src/components/InputBook.jsx
@@ -1,14 +1,26 @@
-import React from "react";
+import React from 'react';
 
 const InputBook = () => (
-    <div className="form-container">
-        <h3 className="form-heading">Add a new book</h3>
-        <form>
-            <input className="title"name="title" type='text' placeholder="Book Title" required />
-            <input className="author"name="author" type='text' placeholder="Author" required />
-            <button className="add-btn">Add Book</button>
-        </form>
-    </div>
+  <div className="form-container">
+    <h3 className="form-heading">Add a new book</h3>
+    <form>
+      <input
+        className="title"
+        name="title"
+        type="text"
+        placeholder="Book Title"
+        required
+      />
+      <input
+        className="author"
+        name="author"
+        type="text"
+        placeholder="Author"
+        required
+      />
+      <button type="button" className="add-btn">Add Book</button>
+    </form>
+  </div>
 );
 
 export default InputBook;
